4bc71a6e77230ac081a1394b869ec4c37f0584eb
seralv <sergioalvaro.14@gmail.com>
2023-09-17 12:59:44 -0400

    Completing basic CRUD

--- tasks/views.py
+++ tasks/views.py
@@ -7,3 +7,7 @@ from .serializers import TaskSerializer
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
